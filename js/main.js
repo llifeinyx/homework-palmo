d64af2442906c66408f4b1cd603fb8713219b6b5
Zhan Boyko <zhan.boyko.2018@gmail.com>
2021-11-23 21:10:45 +0300

    Homework 1 v.1 commit
    First my commit with homework 1.
    Maybe will be changed.

---
+++ js/main.js
@@ -0,0 +1,31 @@
+let num = 10;
+console.log(num);
+let first = 5;
+let second = 10;
+console.log(first + second);
+console.log(first - second);
+console.log(first * second);
+console.log(first / second);
+let bar = 20;
+let qwerty = 5;
+let res = bar - qwerty - 10;
+let end = res - qwerty;
+let str = 'Hello, Palmo.';
+alert(str);
+let name = 'Жан';
+let age = '18';
+alert('Я ' + name + ' мне ' + age + '.');
+function timeCounter() {
+    const SEC_IN_HOUR = 60;
+    const secInDay = SEC_IN_HOUR * 24;
+    const secInMonth = secInDay * 30;
+    console.log('sec in hour ' + SEC_IN_HOUR);
+    console.log('sec in day ' + secInDay);
+    console.log('sec in month ' + secInMonth);
+}
+timeCounter();
+let number = 5;
+number = Math.pow(number, 2);
+let d = 8;
+let n = Math.pow(d, 2);
+let x = Math.pow(d, 3);
