4a988386781970b9ad76ab70b9a3d83a52a81804
zhan <zhan.boyko.2018@gmail.com>
2021-12-26 03:17:24 +0200

    Mega commit for 3rd homework
    /php forms, MORE FORMS/

--- src/public/form.php
+++ src/public/form.php
@@ -1,33 +1,115 @@
 <?php
-if (isset($_POST)){
-    print("Имя: " . $_POST['name']);
-    print("<br>Email: " . $_POST['email'] . "<br>");
+// PHP CODE FROM LESSON
+//if (isset($_POST)){
+//    print("Имя: " . $_POST['name']);
+//    print("<br>Email: " . $_POST['email'] . "<br>");
+//    //var_dump($_POST);
+//}
+//if (isset($_FILES['avatar'])){
+//    $file = $_FILES['avatar'];
+//    print("Загружен файл с именем " .$file['name'] . " и размером " . $file['size'] . " байт");
+//}
+//$current_path = $_FILES['avatar']['tmp_name'];
+//
+//$filename = $_FILES['avatar']['name'];
+//$new_path = dirname(__FILE__) . '/uploads/' . $filename;
+//$file_path = 'uploads/' . $filename;
+//
+//move_uploaded_file($current_path, $new_path);
+//
+
+// PHP CODE FOR HOMEWORK
+
+function hasNumInStr($str){
+    for ($i = 0; $i < strlen($str); $i++){
+        if (is_numeric($str[$i])){
+            return true;
+        }
+    }
+}
+
+function isCorrectString($str, $MODE = 'STRICT'){
+    if ($MODE === 'STRICT'){
+        $treatmentStr = htmlspecialchars(mb_convert_case(mb_strtolower(trim($str)), MB_CASE_TITLE));
+        if (hasNumInStr($treatmentStr) || strlen($treatmentStr) === 0){
+            return 'некорректный ввод';
+        } else {
+            return $treatmentStr;
+        }
+    }
+    if ($MODE === 'NOT_STRICT'){
+        $treatmentStr = htmlspecialchars((trim($str)));
+        if (is_numeric($treatmentStr) || strlen($treatmentStr) === 0){
+            return 'некорректный ввод';
+        } else{
+            return  $treatmentStr;
+        }
+    }
+}
+
+
+
+if (isset($_POST) && isset($_POST['send1Form'])){
+    print ("<h1>ЗАДАНИЕ ДЛЯ ПЕРВОЙ ФОРМЫ</h1>");
+    print('Ваш город: ' . isCorrectString($_POST['enterCity']) . "<br>");
+    print('Ваш возраст: ' . $_POST['radioAge'] . "<br>");
     //var_dump($_POST);
 }
-if (isset($_FILES['avatar'])){
-    $file = $_FILES['avatar'];
-    print("Загружен файл с именем " .$file['name'] . " и размером " . $file['size'] . " байт");
+
+if (isset($_POST) && isset($_POST['send2Form'])){
+    print ("<h1>ЗАДАНИЕ ДЛЯ ВТОРОЙ ФОРМЫ</h1>");
+    print('Название товара: ' . isCorrectString($_POST['enterItemName'], 'NOT_STRICT') . "<br>");
+    print('Производитель товара: ' . isCorrectString($_POST['enterItemCreator'], 'NOT_STRICT') . "<br>");
+    print('Краткое описание товара: ' . isCorrectString($_POST['commentAboutItem'], 'NOT_STRICT') . "<br>");
+    if(isset($_FILES['imageItem'])){
+        $file = $_FILES['imageItem'];
+        print("Загружен файл с именем " .$file['name'] . " и размером " . $file['size'] . " байт");
+
+        $current_path = $_FILES['imageItem']['tmp_name'];
+        //$filename = $_FILES['imageItem']['name'];
+        //$new_path = dirname(__FILE__) . '/uploads/' . $filename;
+        //$file_path = 'uploads/' . $filename;
+        //move_uploaded_file($current_path, $new_path);
+        //echo "<img src='{$file_path}' alt='itemImage'>";
+    }
 }
-$current_path = $_FILES['avatar']['tmp_name'];
-
-$filename = $_FILES['avatar']['name'];
-$new_path = dirname(__FILE__) . '/uploads/' . $filename;
-$file_path = 'uploads/' . $filename;
-
-move_uploaded_file($current_path, $new_path);
+//var_dump($_POST);
+if (isset($_POST) && (isset($_POST['php_x']) || isset($_POST['html_x']))){
+    $mark = 0;
+    print ("<h1>ЗАДАНИЕ ДЛЯ ТРЕТЬЕЙ ФОРМЫ</h1>");
+    $answerOnPutinTest = $_POST['test1'];
+    if (isCorrectString($answerOnPutinTest) === 'Путин'){
+        $mark++;
+        print('Ответ на первый вопрос правильный' . "<br>");
+    } else {
+        print('Ответ на первый вопрос неправильный' . "<br>");
+    }
+    if (isset($_POST['test2'])){
+        if ($_POST['test2'] === '988'){
+            $mark++;
+            print ('Ответ на второй вопрос правильный' . "<br>");
+        } else {
+            print ('Ответ на второй вопрос неправильный' . "<br>");
+        }
+    } else {
+        print ('Ответ на второй вопрос неправильный' . "<br>");
+    }
+    print ('Ваши правильные варианты в 3 тесте:' . "<br>");
+    if (isset($_POST['test3RightAnswer1'])){
+        $mark++;
+        print ('false || true' . "<br>");
+    }
+    if (isset($_POST['test3RightAnswer2'])){
+        $mark++;
+        print ('true && true' . "<br>");
+    }
+    if ( !isset($_POST['test3RightAnswer1']) && !isset($_POST['test3RightAnswer2'])){
+        print ('их нет' . "<br>");
+    }
+    if (isset($_POST['php_x'])){
+        print ('На финальный вопрос вы ответили неправильно. Ваши баллы: ' . $mark);
+    } else {
+        print ('Вы ответили правильно на финальный вопрос. Ваши баллы: ' . ++$mark);
+    }
+}
 ?>
-
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<p>Ваша ава:<br></p>
-<img src="<?php echo $file_path?>" alt="">
-</body>
-</html>
--- src/public/index.php
+++ src/public/index.php
@@ -6,56 +6,147 @@
     <title>Palmo Helloe world</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="stylesheet" href="css/main.css">
     <link rel="manifest" href="site.webmanifest">
     <meta name="theme-color" content="#fafafa">
 </head>
 
 <body style="margin: 0;">
-<form name="feedback" method="POST" action="form.php" enctype="multipart/form-data">
-    <label>Ваше имя: <input type="text" name="name"></label>
-    <label>Ваш email: <input type="text" name="email"></label>
-    <label>Аватарка: <input type="file" name="avatar"></label>
-    <input type="submit" name="send" value="Отправить">
+<form  class="first-form" name="feedback" method="POST" action="form.php" enctype="multipart/form-data">
+    <h1>ФОРМА ДЛЯ ПЕРВОГО ЗАДАНИЯ</h1>
+    <label>
+        В каком городе вы проживаете?
+        <input required type="text" name="enterCity">
+    </label>
+    <label>
+        Сколько вам лет?
+        <div>
+            <p>
+                <input required type="radio" name="radioAge" value="20<">
+                <span>20<</span>
+            </p>
+            <p>
+                <input type="radio" name="radioAge" value="20-25">
+                <span>20 - 25</span>
+            </p>
+            <p>
+                <input type="radio" name="radioAge" value="26-30">
+                <span>26 - 30</span>
+            </p>
+            <p>
+                <input type="radio" name="radioAge" value=">30">
+                <span>>30</span>
+            </p>
+        </div>
+    </label>
+    <input type="submit" name="send1Form" value="Отправить форму">
 </form>
+<form id="itemForm" class="first-form" name="itemCreate" method="POST" action="form.php" enctype="multipart/form-data">
+    <h1>ФОРМА ДЛЯ ВТОРОГО ЗАДАНИЯ</h1>
+    <label>
+        Название товара:
+        <input required type="text" name="enterItemName">
+    </label>
+    <label>
+        Производитель товара:
+        <input required type="text" name="enterItemCreator">
+    </label>
+    <label>
+        Краткое описание:
+        <textarea required name="commentAboutItem" form="itemForm" maxlength="50">
+
+        </textarea>
+    </label>
+    <label>
+        Отправьте фотографию:
+        <input required type="file" name="imageItem" accept=".jpg, .jpeg, .png">
+    </label>
+    <input type="submit" name="send2Form" value="Отправить форму">
+</form>
+<form id="testForm" class="first-form" name="test" method="POST" action="form.php" enctype="multipart/form-data">
+    <h1>ФОРМА ДЛЯ ТРЕТЬЕГО ЗАДАНИЯ</h1>
+    <p>ВНИМАНИЕ! ТЕСТ!</p>
+    <label>
+        Фамилия президента РФ:
+        <input type="text" name="test1">
+    </label>
+    <label>
+        В каком году русь крестили:
+        <input type="radio" name="test2" value="988">
+        <span>988</span>
+        <input type="radio" name="test2" value="989">
+        <span>989</span>
+        <input type="radio" name="test2" value="999">
+        <span>999</span>
+    </label>
+    <label>
+        Какие утверждения равны true:
+        <input type="checkbox" name="test3WrongAnswer1">
+        <span>true && false</span>
+        <input type="checkbox" name="test3RightAnswer1">
+        <span>false || true</span>
+        <input type="checkbox" name="test3RightAnswer2">
+        <span>true && true</span>
+        <input type="checkbox" name="test3WrongAnswer2">
+        <span>false && false</span>
+    </label>
+    <p>ФИНАЛЬНЫЙ ВОПРОС! НАЖАТИЕ НА ОДНУ ИЗ КАРТИНОК ОТПРАВИТ ФОРМУ!</p>
+    <label class="super-class">
+        Какой ЯП убогей?:
+        <input type="image" form="testForm" src="img/php.svg" alt="php" width="150" height="150" name="php">
+        <input type="image" form="testForm" src="img/html.png" alt="html" width="150" height="150" name="html">
+    </label>
+</form>
+
 <?php
-    //ex 1
-    function numIn($num, $pow){
-        return intval($num) ** intval($pow);
-    }
-    echo numIn(3, 9), "<br>";
-    //ex 2
-    function numToWeek($num){
-        $weekDay = ['пон', 'вт','ср' ,'чт','пт','сб','вс'];
-        if ((int)$num < 0 || (int)$num > 7){
-            return 'Некорректный ввод';
-        }
-        return $weekDay[$num];
-    }
-    echo numToWeek(4), "<br>";
-    //ex 3
-    function slug($str){
-        return str_replace(' ', '-', mb_strtolower(trim($str)));
-    }
-    echo slug('Hello palmo'), "<br>";
-    //ex 4
-    $products = [
-        ['name' => 'Телевизор', 'prise' => '400', 'quantity' => 1],
-        ['name' => 'Телефон', 'prise' => '300', 'quantity' => 3],
-        ['name' => 'Кроссовки', 'prise' => '150', 'quantity' => 2]
-    ];
-    function sumAndCount($arr)
-    {
-        $fullPrice = 0;
-        $fullQuantity = 0;
-        foreach ($arr as $i) {
-            $fullPrice += intval($i['prise']);
-            $fullQuantity += intval($i['quantity']);
-        }
-        //echo $fullPrice, ' ', $fullQuantity, "<br>";
-        return [$fullPrice, $fullQuantity];
-    }
-    echo sumAndCount($products)[0], ' ', sumAndCount($products)[1] , "<br>";
+//
+//
+//    /* EX ON LESSON */
+//
+//
+//    //ex 1
+//    function numIn($num, $pow){
+//        return intval($num) ** intval($pow);
+//    }
+//    echo numIn(3, 9), "<br>";
+//    //ex 2
+//    function numToWeek($num){
+//        $weekDay = ['пон', 'вт','ср' ,'чт','пт','сб','вс'];
+//        if ((int)$num < 0 || (int)$num > 7){
+//            return 'Некорректный ввод';
+//        }
+//        return $weekDay[$num];
+//    }
+//    echo numToWeek(4), "<br>";
+//    //ex 3
+//    function slug($str){
+//        return str_replace(' ', '-', mb_strtolower(trim($str)));
+//    }
+//    echo slug('Hello palmo'), "<br>";
+//    //ex 4
+//    $products = [
+//        ['name' => 'Телевизор', 'prise' => '400', 'quantity' => 1],
+//        ['name' => 'Телефон', 'prise' => '300', 'quantity' => 3],
+//        ['name' => 'Кроссовки', 'prise' => '150', 'quantity' => 2]
+//    ];
+//    function sumAndCount($arr)
+//    {
+//        $fullPrice = 0;
+//        $fullQuantity = 0;
+//        foreach ($arr as $i) {
+//            $fullPrice += intval($i['prise']);
+//            $fullQuantity += intval($i['quantity']);
+//        }
+//        //echo $fullPrice, ' ', $fullQuantity, "<br>";
+//        return [$fullPrice, $fullQuantity];
+//    }
+//    echo sumAndCount($products)[0], ' ', sumAndCount($products)[1] , "<br>";
+//
+//
+
+ /* EX HOMEWORK */
+//ex 1
+
 ?>
 </body>
 
