5c242d190536cbaa4c655a224137e1af1790f4f2
Жан Бойко <zhan.boyko.2018@gmail.com>
2022-01-17 19:29:35 +0200

    php6 homework commit from lesson
    (homework after)

--- src/public/home.php
+++ src/public/home.php
@@ -1,15 +1,1 @@
 <?php
-if(isset($_POST) && isset($_POST['send_file'])){
-    $file_path = $_FILES['file']['tmp_name'];
-    foreach(file($file_path) as $str){
-        echo "<pre>" . htmlspecialchars($str) . "</pre>";
-    }
-}
-if (isset($_POST) && isset($_POST['registration'])){
-    $file_name = 'users.txt';
-    $file_pointer = fopen($file_name, 'w+');
-    chmod($file_name, 0777);
-    fwrite($file_pointer, htmlspecialchars(trim($_POST['login'])) . PHP_EOL);
-    fclose($file_pointer);
-    header('Location: index.php');
-}
--- src/public/index.php
+++ src/public/index.php
@@ -17,135 +17,73 @@
 </head>
 
 <body style="margin: 0;">
-<form action="home.php" enctype="multipart/form-data" method="post">
-    <h1>ФОРМА ДЛЯ ЗАГРУЗКИ ФАЙЛА</h1>
-    <label for="file">Загрузите файл формата txt:</label>
-    <input type="file" id="file" name="file" accept=".txt" required>
-    <input type="submit" value="отправить" name="send_file">
-</form>
-<form action="home.php" enctype="multipart/form-data" method="post">
-    <h1>ФОРМА ДЛЯ РЕГИСТРАЦИИ</h1>
-    <label for="login">Введите логин:</label>
-    <input type="text" id="login" name="login" required>
-    <label for="password">Введите пароль:</label>
-    <input type="text" id="password" name="password" required>
-    <input type="submit" value="отправить" name="registration">
-</form>
+<!--<form action="home.php" enctype="multipart/form-data" method="post">-->
+<!--    <h1>ФОРМА ДЛЯ ЗАГРУЗКИ ФАЙЛА</h1>-->
+<!--    <label for="file">Загрузите файл формата txt:</label>-->
+<!--    <input type="file" id="file" name="file" accept=".txt" required>-->
+<!--    <input type="submit" value="отправить" name="send_file">-->
+<!--</form>-->
+
 <?php
-//FROM PAST LESSON
-
-//$file1 = fopen('text.txt', 'w+');
-//fwrite($file1, 'Hello Palmo');
-////$txt = fread($file1, filesize('text.txt'));
-//$txt = file_get_contents('text.txt');
-////var_dump($txt);
-//echo $txt . "<br>";
-//echo stat('text.txt')['size'] . ' размер в байтах ' . "<br>";
-//echo (stat('text.txt')['size']/1000) . ' размер в мегабайтах ' . "<br>";
-//echo (stat('text.txt')['size']/10000) . ' размер в гигабайтах ' . "<br>";
-//$file2 = fopen('text2.txt', 'w+');
-//unlink('text2.txt');
-//
-//function dirOrFile($dirOrFile){
-//    if (is_dir($dirOrFile)){
-//        return 'is dir';
-//    } elseif(is_file($dirOrFile)) {
-//        return "is file";
-//    } else {
-//        return 'wtf';
-//    }
-//}
-//
-//$dirNameArr = ['text1', 'text2', 'text3'];
-//if (!file_exists('Test')){
-//    mkdir('Test', 0777);
-//}
-//foreach ($dirNameArr as $name){
-//    if (!file_exists('Test/' . $name)){
-//        mkdir(('Test/' . $name), 0777);
-//    }
-//    $fileTmp = fopen('Test/' . $name . '/Hello.txt', 'w+');
-//    fwrite($fileTmp, 'Hello Palmo!');
-//    echo file_get_contents('Test/' . $name . '/Hello.txt');
-//}
-//    echo dirOrFile('jjjjjjjjjjj') . "<br>";
-//
-//    function printFileFromDir($mainDirPath){
-//        $sumFile = 0;
-//        $scanDir = scandir($mainDirPath);
-//        foreach ($scanDir as $dir){
-//            if ($dir === '.' || $dir === '..'){
-//                continue;
-//            }
-//            if (is_file($mainDirPath . '/' . $dir)){
-//                $sumFile += filesize($mainDirPath . '/' . $dir);
-//            } elseif(is_dir($mainDirPath . '/' . $dir)) {
-//                $sumFile += printFileFromDir($mainDirPath . '/' . $dir);
-//            }
-//        }
-//        return $sumFile;
-//    }
-//    function deleteNahoy($mainDirPath){
-//        $scanDir = scandir($mainDirPath);
-//        foreach ($scanDir as $dir){
-//            if ($dir === '.' || $dir === '..'){
-//                continue;
-//            }
-//            if (is_file($mainDirPath . '/' . $dir)){
-//                unlink($mainDirPath . '/' . $dir);
-//            } elseif(is_dir($mainDirPath . '/' . $dir)) {
-//                deleteNahoy($mainDirPath . '/' . $dir);
-//                //echo
-//                rmdir($mainDirPath . '/' . $dir);
-//            }
-//        }
-//    }
-//    echo printFileFromDir('Test');
-//    deleteNahoy('Test');
-
-// HOMEWORK
-
-// main data
-$file_name = 'fruits.txt';
-$fruits_file = fopen($file_name, 'w+');
-$fruits_string = 'apple' . PHP_EOL . 'banana' . PHP_EOL . 'cherry' . PHP_EOL . 'tomato' . PHP_EOL . 'cucumber'
-    . PHP_EOL . 'orange' . PHP_EOL . 'red' . PHP_EOL . 'chess' . PHP_EOL . 'borsh' . PHP_EOL . 'green';
-// init file
-//chmod($file_name, 0777);
-fwrite($fruits_file, $fruits_string);
-// close file
-fclose($fruits_file);
-
-//function sort (1 ex)
-function sortFile($file_name){
-    $file_pointer = fopen($file_name, 'r+');
-    $strArray = file($file_name);
-    sort($strArray, SORT_STRING);
-    foreach ($strArray as $str){
-        fwrite($file_pointer, $str);
-    }
-    fclose($file_pointer);
-}
-sortFile($file_name);
-
-//function for delete file by rule > 1 mb (last ex)
-function deleteFileBySize(int $size, string $dir_path){
-    $scan_list = scandir($dir_path);
-    foreach ($scan_list as $item){
-        if ($item === '.' || $item === '..'){
-            continue;
+    class Calc{
+        protected $n1;
+        protected $n2;
+        public function __construct($n1, $n2)
+        {
+            $this->n1 = $n1;
+            $this->n2 = $n2;
         }
-        if (is_file($dir_path . '/' . $item) && filesize($dir_path . '/' . $item) > $size){
-            unlink($dir_path . '/' . $item);
+        public function sum()
+        {
+            echo "Sum n=" , $this->n1 + $this->n2 , "<br>";
         }
-        if (is_dir($dir_path . '/' . $item)){
-            deleteFileBySize($size, $dir_path . '/' . $item);
+        public function minus()
+        {
+            echo "Minus n=" ,$this->n1 - $this->n2 , "<br>";
+        }
+        public function multiply()
+        {
+            return $this->n1 * $this->n2;
+        }
+        public function delenie()
+        {
+            echo "delenie n=" , $this->n1 / $this->n2 , "<br>";
         }
     }
-}
+    $lol = new Calc(4, 2);
+    $lol->sum();
+    $lol->minus();
+    $lol->multiply();
+    $lol->delenie();
 
-// 1 mb = 10000 b
-deleteFileBySize(10000, 'uploads');
+
+    class Worker{
+        public $name;
+        public $age;
+        public $salary;
+    }
+    $obj1 = new Worker();
+    $obj1->name = 'Иван';
+    $obj1->age = 25;
+    $obj1->salary = 1000;
+    $obj2 = new Worker();
+    $obj2->name = 'Вася';
+    $obj2->age = 26;
+    $obj2->salary = 2000;
+    echo $obj1->salary + $obj2->salary, "<br>";
+    echo $obj1->age + $obj2->age, "<br>";
+    class Pow extends Calc{
+        public function __construct($n1, $n2)
+        {
+            parent::__construct($n1, $n2);
+        }
+        public function pow()
+        {
+            echo 'ultra pow=' , $this->multiply() * $this->multiply(), "<br>";
+        }
+    }
+    $lf = new Pow(2, 3);
+    $lf->pow();
 ?>
 </body>
 
