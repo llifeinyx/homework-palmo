af4ceffbc6f38411d4127c3245600f9646b2ed8a
Жан Бойко <zhan.boyko.2018@gmail.com>
2022-01-26 17:41:46 +0200

    PHP LARAVEL 1
    f;l

--- src/public/index.php
+++ src/public/index.php
@@ -1,24 +1,55 @@
-<!doctype html>
-<html class="no-js" lang="">
+<?php
 
-<head>
-    <meta charset="utf-8">
-    <title>Palmo Helloe world</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="manifest" href="site.webmanifest">
-    <meta name="theme-color" content="#fafafa">
-</head>
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
-<body style="margin: 0;">
-    <div>
-        <a href="authorization.php">Авторизироваться</a>
-    </div>
-    <div>
-        <a href="registration.php">Зарегестрироваться</a>
-    </div>
-    <div><?php echo $_COOKIE['success-registration'] ?? "";?></div>
-</body>
+define('LARAVEL_START', microtime(true));
 
-</html>
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
+
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
