574c7ada1620b061818dda577629dcfa08612bdf
Жан Бойко <zhan.boyko.2018@gmail.com>
2022-01-26 19:18:36 +0200

    PHP LARAVEL 1.1
    f;l

---
+++ src/resources/views/home.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        .tic {
+            display: grid;
+            grid-template-columns: 100px 100px 100px;
+        }
+    </style>
+</head>
+<body>
+<h1>TIC TAC TOE</h1>
+<div class="tic">
+    <a href="#">.</a>
+    <a href="#">.</a>
+    <a href="#">.</a>
+</div>
+<div class="tic">
+    <a href="#">.</a>
+    <a href="#">.</a>
+    <a href="#">.</a>
+</div>
+<div class="tic">
+    <a href="#">.</a>
+    <a href="#">.</a>
+    <a href="#">.</a>
+</div>
+</body>
+</html>
